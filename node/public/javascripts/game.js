beb5f93b2a10bdb82976ac9d23e5cb47fb267cb3
Dirk <dirk@popskorn.de>
2016-11-12 13:26:41 +0100

    working on responsive table

--- node/public/javascripts/game.js
+++ node/public/javascripts/game.js
@@ -1,11 +1,11 @@
 var socket = io();
 
-socket.on('connect', function(s) {
+socket.on('connect', function (s) {
 
     // Parse game name and user id from current URL
     var path = window.location.pathname.split('/');
-    var player_id = path[path.length-1];
-    var game_name = path[path.length-2];
+    var player_id = path[path.length - 1];
+    var game_name = path[path.length - 2];
 
     console.log('Server connection established');
     console.log('Sending init message to server...');
@@ -18,7 +18,7 @@ socket.on('connect', function(s) {
 });
 
 // Server sends an update to the game board (words, crossout state, etc...). This is then rendered to the dom
-socket.on('game_update', function(data) {
+socket.on('game_update', function (data) {
 
     console.log('Received: game_update');
 
@@ -29,31 +29,34 @@ socket.on('game_update', function(data) {
     // Render the words into the table. The permutation array at position i specifies the index of the word in the words
     // array to be rendered.
     // Also mark some cells as crossed out according to the data sent from the server.
-    $('#game-table td').each(function(i) {
+    $('#game-table td').each(function (i) {
+    //$('div.square').each(function (i) {
 
         $(this).html(words[permutation[i]]);
 
-        if(score_matrix[i] == '0' && $(this).hasClass('crossout')) {
+        if (score_matrix[i] == '0' && $(this).hasClass('crossout')) {
             $(this).removeClass('crossout');
         }
-        if(score_matrix[i] == '1' && !$(this).hasClass('crossout')) {
+        if (score_matrix[i] == '1' && !$(this).hasClass('crossout')) {
             $(this).addClass('crossout');
         }
     });
 });
 
-socket.on('status_update', function(data) {
-   console.log(data);
-    $('ul#player-list').empty()
-    data.forEach(function(player) {
-        $('ul#player-list').append('<li>' + player.player_name +  (player.is_online ? '(online)' : '(offline)') + '</li>');
+socket.on('status_update', function (data) {
+    active_players = data.active_players;
+
+    $('ul#player-list').empty();
+
+    active_players.forEach(function (player) {
+        $('ul#player-list').append('<li>' + player.player_name + ' (' + player.num_hits + ' Treffer) ' + (player.is_online ? '(online)' : '(offline)') + '</li>');
     });
 });
 
 function getScoreMatrix() {
     var ret = '';
-    $('#game-table td').each(function(i) {
-        if($(this).hasClass('crossout'))
+    $('#game-table td').each(function (i) {
+        if ($(this).hasClass('crossout'))
             ret += '1'
         else
             ret += '0'
@@ -62,10 +65,10 @@ function getScoreMatrix() {
 }
 
 // UI-triggered functions
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Toogle the crossout animation locally and send a notification to the server, that the cell was crossed out.
-    $('#game-table td').click(function() {
+    $('#game-table td').click(function () {
         $(this).toggleClass('crossout');
         socket.emit('score_update', getScoreMatrix());
     });
--- node/sockets/base.js
+++ node/sockets/base.js
@@ -1,16 +1,9 @@
 module.exports = function (io) {
 
-    function sendGameDataToClient(game, player, socket) {
-        socket.emit('game_update', {
-            game: game,
-            player: player
-        });
-    }
-
     function sendStatusUpdateToAllPlayers(game, player, is_online) {
 
         // Remove player from Game entry if already exists
-        game.active_players = game.active_players.filter(function(element){
+        game.active_players = game.active_players.filter(function (element) {
             return element.player_id != String(player._id);
         });
 
@@ -18,19 +11,25 @@ module.exports = function (io) {
         game.active_players.push({
             player_name: player.name,
             player_id: String(player._id),
-            is_online: is_online
+            is_online: is_online,
+            num_hits: player.score_matrix.split('1').length - 1
         });
         game.save();
 
         // Send update to all connected clients (all games)
-        io.emit('status_update', game.active_players);
+        io.emit('status_update', {
+            game_id: String(game._id),
+            game_name: game.name,
+            active_players: game.active_players
+        });
     }
 
     // Load models
     var Game = require('../models/game');
     var Player = require('../models/player');
 
-    io.on('connection', function(socket) {
+    // A new user connects
+    io.on('connection', function (socket) {
         console.log('A client connected');
 
         var game_name = undefined;
@@ -38,88 +37,91 @@ module.exports = function (io) {
 
         // When a client loads a game board, it sends an init message containing the game- and player ids parsed from
         // the pages url
-        socket.on('init', function(data) {
+        socket.on('init', function (data) {
             game_name = data.game_name;
             player_id = data.player_id;
 
             console.log('Client sent init message: ' + player_id + ' on game ' + game_name);
 
             // Lookup if the specified game and player exist in the database
-            Game.findOne({ name: game_name}, function(err, game) {
-                if(err || !game) {
+            Game.findOne({name: game_name}, function (err, game) {
+                if (err || !game) {
                     console.log('init: Requested game does not exists in the database');
                     return;
                 }
                 else {
                     // Look up requested player in the database
-                    Player.findById(player_id, function(err, player) {
-                        if(err || !player)
+                    Player.findById(player_id, function (err, player) {
+                        if (err || !player)
                             console.log('Requested player does not exists in the database');
                         else {
-                            console.log('Found requested game and player in the database');
+                            // Send game data to client
+                            socket.emit('game_update', {
+                                game: game,
+                                player: player
+                            });
 
-                            // Now send game data to client
-                            sendGameDataToClient(game, player, socket);
-
+                            // Notify all players about the new player
                             sendStatusUpdateToAllPlayers(game, player, true);
-
-                            console.log(game.active_players);
                         }
                     });
                 }
             });
         });
 
-        socket.on('score_update', function(data) {
-            if(!game_name || !player_id) {
-               console.log('Error. score_update received, but socket was not initialized');
-               return;
+        socket.on('score_update', function (data) {
+            if (!game_name || !player_id) {
+                console.log('Error. score_update received, but socket was not initialized');
+                return;
             }
             console.log('score_update from player ' + player_id + ': ' + data);
 
-            Player.findById(player_id, function(err, player) {
-               if(err || !player) {
-                   console.log('score_update: cannot find player in database. Aborting.');
-                   return;
-               }
+            Player.findById(player_id, function (err, player) {
+                if (err || !player) {
+                    console.log('score_update: cannot find player in database. Aborting.');
+                    return;
+                }
 
-               player.score_matrix = data;
-               player.save();
+                player.score_matrix = data;
+                player.save();
+
+                Game.findOne({ name: game_name }, function(err, game) {
+                    if(err || !game) {
+                        console.log('score_update: game not found. Aborting');
+                        return;
+                    }
+
+                    // Notify all players about the changes score
+                    sendStatusUpdateToAllPlayers(game, player, true);
+
+                });
             });
-
         });
 
-        socket.on('disconnect', function() {
+        socket.on('disconnect', function () {
             console.log('A player disconnected');
 
-            if(!player_id) {
-                console.log('disconnect: player_id was not set. Aborting');
+            if (!player_id || !game_name) {
+                console.log('disconnect: player_id or game_name was not set. Aborting');
                 return;
             }
 
-            if(!game_name) {
-                console.log('disconnect: game_name was not set. Aborting');
-                return;
-            }
+            // Get game, player and send updated online status to all connected clients
+            Game.findOne({name: game_name}, function (err, game) {
+                if (err || !game) {
+                    console.log('disconnect: cannot find game in database. Aborting.');
+                    return;
+                }
 
-            Game.findOne({ name: game_name}, function(err, game) {
-               if(err || !game) {
-                   console.log('disconnect: cannot find game in database. Aborting.');
-                   return;
-               }
+                Player.findById(player_id, function (err, player) {
+                    if (err || !player) {
+                        console.log('disconnect: cannot find player. Aborting');
+                        return;
+                    }
 
-               Player.findById(player_id, function(err, player) {
-                   if(err || !player) {
-                       console.log('disconnect: cannot find player. Aborting');
-                       return;
-                   }
-
-                   sendStatusUpdateToAllPlayers(game, player, false);
-               })
-
+                    sendStatusUpdateToAllPlayers(game, player, false);
+                })
             });
-
-
         });
     });
 };
