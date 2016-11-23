370f70d342a18bede665adc563cbbb4632bdc417
Dirk <dirk@popskorn.de>
2016-11-23 19:57:39 +0100

    somewhat working facebook auth

--- node/app.js
+++ node/app.js
@@ -4,8 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+// Facebook login credentials
+var FACEBOOK_APP_ID = '928723270595945';
+var FACEBOOK_APP_SECRET = '2d47fc49af0d741c56c2680a5ba62820';
 
 var app = express();
 
@@ -17,17 +23,41 @@ app.set('view engine', 'handlebars');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//
+// Facebook authentication middleware
+//
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+}, function (accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+        //Assuming user exists
+        done(null, profile);
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
 
 // Load database connection
 mongoose.connect('mongodb://mongo/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function() {
-  console.log('Database connection established.');
+db.once('open', function () {
+    console.log('Database connection established.');
 });
 
 // Routing setup
@@ -42,10 +72,10 @@ app.use('/live', live);
 app.use('/auth', auth);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -53,23 +83,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
--- node/routes/auth.js
+++ node/routes/auth.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 // Load models
 var Player = require('../models/player');
@@ -11,5 +12,20 @@ var Game = require('../models/game');
 router.get('/', function (req, res, next) {
     res.render('auth', {});
 });
+
+router.get('/facebook', passport.authenticate('facebook'));
+
+router.get('/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: '/auth/success',
+    failureRedirect: '/auth/error'
+}));
+
+router.get('/success', function(req, res, next) {
+    res.send('Success');
+})
+
+router.get('/error', function(req, res, next) {
+    res.send('Error');
+})
 
 module.exports = router;
