d850bffb8babc025f089fecdd8e29d6771995ae4
Dirk <dirk@popskorn.de>
2016-11-24 19:11:48 +0100

    moved config to external file

--- node/config.js
+++ node/config.js
@@ -5,7 +5,6 @@ config.FACEBOOK_APP_SECRET          = '2d47fc49af0d741c56c2680a5ba62820';
 config.FACEBOOK_LOCAL_CALLBACK_URL  = 'http://localhost:3000/auth/facebook/callback';
 config.FACEBOOK_REMOTE_CALLBACK_URL = 'http://bingo.onehot.de/auth/facebook/callback';
 
-
 config.SESSION_SECRET               = '42';
 
 config.DATABASE_URL                 = 'mongodb://mongo/test';
